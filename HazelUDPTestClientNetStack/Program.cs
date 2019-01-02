d1867d86b0f58faa49dbd4d82324d930eb7f6bfb
Enrico Speranza <enrico.speranza@gmail.com>
2019-01-02 15:51:25 +0100

    Add test with NetStack

---
+++ HazelUDPTestClientNetStack/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HazelUDPTestClientNetStack
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
